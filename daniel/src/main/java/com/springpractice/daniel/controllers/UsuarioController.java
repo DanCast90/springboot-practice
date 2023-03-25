f94d775f0e98bffd0322bb0abe231fe56575709b
DanCast90 <ternuraxdxd@gmail.com>
2023-03-24 22:14:28 -0600

    create controllers to consult users

--- daniel/src/main/java/com/springpractice/daniel/controllers/UsuarioController.java
+++ daniel/src/main/java/com/springpractice/daniel/controllers/UsuarioController.java
@@ -1,17 +1,57 @@
 package com.springpractice.daniel.controllers;
 
+import com.springpractice.daniel.models.Usuario;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "prueba")
-    public String prueba(){
-        return "prueba";
+    @RequestMapping(value = "getU/{id}")
+    public Usuario getUsuario(@PathVariable Long id){
+        Usuario usuario=new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("daniel");
+        usuario.setApellido("de la rosa");
+        usuario.setEmail("correo@correo");
+        usuario.setTelefono("telefono");
+        return usuario;
     }
 
+    @RequestMapping(value = "getUs/")
+    public List<Usuario> getUsuarios(){
+        Usuario usuario=new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("daniel");
+        usuario.setApellido("de la rosa");
+        usuario.setEmail("correo@correo");
+        usuario.setTelefono("telefono");
+        return usuario;
+    }
+
+    @RequestMapping(value = "updU")
+    public Usuario updateUsuario(){
+        Usuario usuario=new Usuario();
+        usuario.setNombre("daniel");
+        usuario.setApellido("de la rosa");
+        usuario.setEmail("correo@correo");
+        usuario.setTelefono("telefono");
+        return usuario;
+    }
+
+    @RequestMapping(value = "delU")
+    public Usuario deleteUsuario(){
+        Usuario usuario=new Usuario();
+        usuario.setNombre("daniel");
+        usuario.setApellido("de la rosa");
+        usuario.setEmail("correo@correo");
+        usuario.setTelefono("telefono");
+        return usuario;
+    }
 
 
 }
