b04a29d983f58a0944bf28082da69ad3169920d8
DanCast90 <ternuraxdxd@gmail.com>
2023-03-24 22:56:14 -0600

    create method to add users in the table

--- daniel/src/main/resources/static/js/usuarios.js
+++ daniel/src/main/resources/static/js/usuarios.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
 async function loadUsers(){
 
-  const request = await fetch('getU/2344', {
+  const request = await fetch('getUs', {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -15,7 +15,16 @@ async function loadUsers(){
     }
   });
   const usuarios = await request.json();
-  console.log(usuarios);
-
+  let listadoHTML='';
+  usuarios.forEach(element => {
+    let users=' <tr><td>'+element.id+'</td><td>'+element.nombre+' '+element.apellido+
+    '</td><td>'+element.email+'</td><td>'+element.telefono+
+    '</td><td><a class="btn btn-warning btn-circle btn-sm mr-1">'+
+    '<i class="fas fa-pen"></i></a><a class="btn btn-danger btn-circle btn-sm">'+
+    '<i class="fas fa-trash"></i></a></td></tr>';
+    listadoHTML+=users;
+  });
+  document.querySelector('#tabla-usuarios tbody').outerHTML = listadoHTML;;
+ 
 
 }
