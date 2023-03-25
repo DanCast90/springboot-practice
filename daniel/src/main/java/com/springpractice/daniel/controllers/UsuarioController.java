bdb812563b13c521857132097688aec47b3859f3
DanCast90 <ternuraxdxd@gmail.com>
2023-03-24 21:13:45 -0600

    add sb-admin 2 template

---
+++ daniel/src/main/java/com/springpractice/daniel/controllers/UsuarioController.java
@@ -0,0 +1,15 @@
+package com.springpractice.daniel.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioController {
+
+    @RequestMapping(value = "prueba")
+    public String prueba(){
+        return "prueba";
+    }
+
+}
